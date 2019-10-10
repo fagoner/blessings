18437dc2dbead0d2b1d645f79a805717316c80ed
David Recinos <drecinos@Davids-MacBook-Pro.local>
2019-10-09 18:34:29 -0600

    First commit

---
+++ src/main/kotlin/com/demo/blessings/BlessingsApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.blessings
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlessingsApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlessingsApplication>(*args)
+}
---
+++ src/test/kotlin/com/demo/blessings/BlessingsApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.demo.blessings
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BlessingsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
