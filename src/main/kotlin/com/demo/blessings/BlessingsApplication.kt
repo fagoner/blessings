7d9f84fa68da63acd9bfd7c44e3b9eecbd7cf773
David Recinos <farendaway@gmail.com>
2019-10-22 19:20:33 -0600

    Testing a new way

--- src/main/kotlin/com/demo/blessings/BlessingsApplication.kt
+++ src/main/kotlin/com/demo/blessings/BlessingsApplication.kt
@@ -1,11 +1,33 @@
 package com.demo.blessings
 
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.HttpInputMessage
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.xml.ws.Response
 
 @SpringBootApplication
 class BlessingsApplication
 
 fun main(args: Array<String>) {
+	Database.connect(url = "jdbc:mysql://mysql:3306/BLESSINGS",
+			driver = "com.mysql.cj.jdbc.Driver",
+			user = "blessUser",
+			password = "bl355")
 	runApplication<BlessingsApplication>(*args)
 }
+
+object Bless: Table() {
+	val id = integer(name = "id").autoIncrement().primaryKey()
+	val message = varchar(length = 60, name = "message")
+}
+
+data class BlessResponse(val id: Int, val message: String)
--- src/test/kotlin/com/demo/blessings/BlessingsApplicationTests.kt
+++ src/test/kotlin/com/demo/blessings/BlessingsApplicationTests.kt
@@ -1,5 +1,10 @@
 package com.demo.blessings
 
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
@@ -9,8 +14,29 @@ import org.springframework.test.context.junit4.SpringRunner
 @SpringBootTest
 class BlessingsApplicationTests {
 
+	companion object{
+
+		@JvmStatic
+		@BeforeClass
+		fun initialize() {
+			Database.connect(url = "jdbc:mysql://mysql:3306/BLESSINGS",
+					driver = "com.mysql.cj.jdbc.Driver",
+					user = "blessUser",
+					password = "bl355")
+		}
+	}
+
+
 	@Test
-	fun contextLoads() {
+	fun gettingAll() {
+		transaction {
+			Bless.select {
+				Bless.id.eq(1)
+			}.first()
+					.run {
+				assert(this[Bless.message] == "Have a nice day")
+			}
+		}
 	}
 
 }
